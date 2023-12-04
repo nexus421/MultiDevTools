9f40320f301ca389a5325fb07af2ca860d643619
Marvin <marvin.kickner@gmail.com>
2023-12-04 19:15:05 +0100

    Optimized HashWindowElement

--- src/main/kotlin/elements/HashWindowElement.kt
+++ src/main/kotlin/elements/HashWindowElement.kt
@@ -1,17 +1,21 @@
 package elements
 
+import HashAlgorithm
 import SplittedWindow
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import hash
 import rememberIt
+import toClipboard
 
 class HashWindowElement : WindowElement() {
     override val name = "Hash"
@@ -20,21 +24,62 @@ class HashWindowElement : WindowElement() {
     override fun windowComposable() {
         var rawInput by rememberIt("")
         var hashedInput by rememberIt("")
+        var selectedHashAlgorithm by rememberIt(HashAlgorithm.MD5)
+        var expandDropDown by rememberIt(false)
+        var checkRealtime by rememberIt(false)
+        var checkClipboard by rememberIt(true)
 
         SplittedWindow(leftSide = {
-            TextField(rawInput, modifier = Modifier.fillMaxSize(), onValueChange = {
+            TextField(rawInput, placeholder = {
+                Text("String to hash")
+            }, modifier = Modifier.fillMaxSize(), onValueChange = {
                 rawInput = it
-                //ToDo: Button: Realtime-Hashen
-                //hashedInput = rawInput.hash()
+                if (checkRealtime) hashedInput = rawInput.hash(selectedHashAlgorithm)
             })
         }, middle = {
+
+            Box {
+                OutlinedButton(modifier = Modifier.fillMaxWidth(), onClick = {
+                    expandDropDown = expandDropDown.not()
+                }) {
+                    Text(selectedHashAlgorithm.algorithm)
+                }
+                DropdownMenu(expandDropDown, onDismissRequest = { expandDropDown = false }) {
+                    HashAlgorithm.entries.forEach {
+                        DropdownMenuItem(onClick = {
+                            selectedHashAlgorithm = it
+                            expandDropDown = false
+                        }) {
+                            Text(it.algorithm)
+                        }
+                    }
+                }
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text("Realtime", modifier = Modifier.weight(1f))
+                Checkbox(checkRealtime, onCheckedChange = {
+                    checkRealtime = checkRealtime.not()
+                })
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text("Copy to clipboard", modifier = Modifier.weight(1f))
+                Checkbox(checkClipboard, onCheckedChange = {
+                    checkClipboard = checkClipboard.not()
+                })
+            }
+
             Button(modifier = Modifier.fillMaxWidth(), onClick = {
-                hashedInput = rawInput.hash()
+                hashedInput = rawInput.hash(selectedHashAlgorithm)
+                if (checkClipboard) hashedInput.toClipboard()
             }) {
                 Text("Hash")
             }
         }, rightSide = {
-            Text(hashedInput, modifier = Modifier.fillMaxSize())
+            SelectionContainer {
+                Text(hashedInput, modifier = Modifier.fillMaxSize())
+            }
         })
     }
 }
