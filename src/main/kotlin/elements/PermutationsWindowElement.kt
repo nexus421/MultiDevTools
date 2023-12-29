b3a3d779495d7a32e03ed3f193e68071331112eb
Marvin <kickner96@gmail.com>
2023-12-29 12:52:36 +0100

    PermutationsWindowElement Idea

---
+++ src/main/kotlin/elements/PermutationsWindowElement.kt
@@ -0,0 +1,12 @@
+package elements
+
+import androidx.compose.runtime.Composable
+
+class PermutationsWindowElement : WindowElement() {
+    override val name = "Permutations"
+
+    @Composable
+    override fun windowComposable() {
+        TODO("Copy from BruteForceWindowElement. Without the BruteForce callback an print all results to the right side!")
+    }
+}
