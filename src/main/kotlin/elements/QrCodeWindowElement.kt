d78bfe6cbbfc9abe531e5f2058766afb31c1326a
Marvin <marvin@kickner.bayern>
2024-03-25 18:14:35 +0100

    QrCodeWindowElement converted to new DefaultSplitWindowElement

--- src/main/kotlin/elements/QrCodeWindowElement.kt
+++ src/main/kotlin/elements/QrCodeWindowElement.kt
@@ -2,104 +2,97 @@ package elements
 
 import ButtonText
 import CheckBoxText
-import SplittedWindow
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asComposeImageBitmap
 import androidx.compose.ui.unit.sp
-import elements.base.WindowElement
+import elements.base.DefaultSplitWindowElement
 import org.jetbrains.skia.Bitmap
 import org.jetbrains.skia.Image
 import org.jetbrains.skiko.toBufferedImage
 import qrcode.QRCode
-import rememberIt
 import utils.TransferableImage
 import utils.copyToClipboard
 import java.awt.Toolkit
 
-class QrCodeWindowElement : WindowElement() {
+class QrCodeWindowElement : DefaultSplitWindowElement() {
     override val name = "QR-Code generator"
 
-    @Composable
-    override fun windowComposable() {
+    private var qrInput by mutableStateOf("")
+    private var qrBitmap by mutableStateOf<Bitmap?>(null)
+    private var liveGeneration by mutableStateOf(false)
+    private var expandDropDown by mutableStateOf(false)
+    private var selectedType by mutableStateOf(Types.Rounded)
+    private var hexColor by mutableStateOf("ffffff")
 
-        var qrInput by rememberIt("")
-        var qrBitmap by rememberIt<Bitmap?>(null)
-        var liveGeneration by rememberIt(false)
-        var expandDropDown by rememberIt(false)
-        var selectedType by rememberIt(Types.Rounded)
-        var hexColor by rememberIt("ffffff")
-
-        SplittedWindow(leftSide = {
-            OutlinedTextField(qrInput, modifier = Modifier.fillMaxWidth(), onValueChange = {
-                qrInput = it
-                if (liveGeneration) {
-                    generateQrCode(qrInput, selectedType, hexColor) {
-                        qrBitmap = it
-                        val image = TransferableImage(it.toBufferedImage())
-                        Toolkit.getDefaultToolkit().systemClipboard.setContents(image, image)
-                    }
-                }
-            }, label = { Text("QR-Code data") })
-        }, middle = {
-            ButtonText("Generate", modifier = Modifier.fillMaxWidth(), onClick = {
-                generateQrCode(qrInput, selectedType, hexColor) { qrBitmap = it }
-            })
-            CheckBoxText("Live generation", liveGeneration) { liveGeneration = it }
-
-//            OutlinedTextField(hexColor, modifier = Modifier.fillMaxWidth(), onValueChange = {
-//                hexColor = it
-//                if (liveGeneration) {
-//                    generateQrCode(qrInput, selectedType, hexColor) {
-//                        qrBitmap = it
-//                    }
-//                }
-//            }, label = { Text("QR-Code hex-color") })
-
-            Box {
-                OutlinedButton(modifier = Modifier.fillMaxWidth(), onClick = {
-                    expandDropDown = expandDropDown.not()
-                }) {
-                    Text(selectedType.name, modifier = Modifier.weight(1f))
-                    Icon(Icons.Default.ArrowDropDown, contentDescription = "DropDown")
-                }
-                DropdownMenu(expandDropDown, onDismissRequest = { expandDropDown = false }) {
-                    Types.entries.forEach {
-                        DropdownMenuItem(onClick = {
-                            selectedType = it
-                            expandDropDown = false
-                            generateQrCode(qrInput, selectedType, hexColor) {
-                                qrBitmap = it
-                            }
-                        }) {
-                            Text(it.name)
-                        }
-                    }
+    override fun getLeftSide(): @Composable BoxScope.() -> Unit = {
+        OutlinedTextField(qrInput, modifier = Modifier.fillMaxWidth(), onValueChange = {
+            qrInput = it
+            if (liveGeneration) {
+                generateQrCode(qrInput, selectedType, hexColor) {
+                    qrBitmap = it
+                    val image = TransferableImage(it.toBufferedImage())
+                    Toolkit.getDefaultToolkit().systemClipboard.setContents(image, image)
                 }
             }
-
-            qrBitmap?.let {
-                Button({ it.copyToClipboard() }, Modifier.fillMaxWidth()) { Text("Copy to Clipboard") }
-            }
-
-            Spacer(Modifier.weight(1f))
-            Text("Based on https://github.com/g0dkar/qrcode-kotlin", fontSize = 10.sp)
-        }, rightSide = {
-            qrBitmap?.let {
-                Image(it.asComposeImageBitmap(), null)
-
-            }
-        })
+        }, label = { Text("QR-Code data") })
     }
+
+    override fun getRightSide(): @Composable BoxScope.() -> Unit = {
+        qrBitmap?.let {
+            Image(it.asComposeImageBitmap(), null)
+
+        }
+    }
+
+    override fun getMiddle(): @Composable ColumnScope.() -> Unit = {
+        ButtonText("Generate", modifier = Modifier.fillMaxWidth(), onClick = {
+            generateQrCode(qrInput, selectedType, hexColor) { qrBitmap = it }
+        })
+        CheckBoxText("Live generation", liveGeneration) { liveGeneration = it }
+
+        Box {
+            OutlinedButton(modifier = Modifier.fillMaxWidth(), onClick = {
+                expandDropDown = expandDropDown.not()
+            }) {
+                Text(selectedType.name, modifier = Modifier.weight(1f))
+                Icon(Icons.Default.ArrowDropDown, contentDescription = "DropDown")
+            }
+            DropdownMenu(expandDropDown, onDismissRequest = { expandDropDown = false }) {
+                Types.entries.forEach {
+                    DropdownMenuItem(onClick = {
+                        selectedType = it
+                        expandDropDown = false
+                        generateQrCode(qrInput, selectedType, hexColor) {
+                            qrBitmap = it
+                        }
+                    }) {
+                        Text(it.name)
+                    }
+                }
+            }
+        }
+
+        qrBitmap?.let {
+            Button({
+                it.copyToClipboard()
+                postSuccessMessage("Copied image to clipboard")
+            }, Modifier.fillMaxWidth()) { Text("Copy to Clipboard") }
+        }
+
+        Spacer(Modifier.weight(1f))
+        Text("Based on https://github.com/g0dkar/qrcode-kotlin", fontSize = 10.sp)
+    }
+
+    override fun getTopView() = null
 
     private fun generateQrCode(input: String, types: Types, hexColor: String, onResult: (Bitmap) -> Unit) {
         val qrRenderedPNG = QRCode.let {
